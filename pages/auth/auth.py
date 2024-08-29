c68e3195c933ec1f5527a923171823d438dac705
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-29 16:49:40 +0500

    Change func. Func name is create_employee_table

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -1,11 +1,32 @@
+import threading
+
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Auth:
-    @log_decorator
-    def logout(self):
-        pass
+    def __init__(self):
+        self.__admin_username = 'admin'
+        self.__admin_password = 'admin'
 
     @log_decorator
     def create_employee_table(self):
+        query = '''
+        CREATE TABLE IF NOT EXISTS employees (
+        ID BIGSERIAL PRIMARY KEY,
+        FIRST_NAME VARCHAR(60) NOT NULL,
+        LAST_NAME VARCHAR(60) NOT NULL,
+        EMAIL VARCHAR(60) NOT NULL UNIQUE,
+        PASSWORD VARCHAR(256) NOT NULL,
+        COMPANY_ID BIGINT REFERENCES companies(ID) NOT NULL,
+        DEPARTMENT BIGINT REFERENCES departments(ID) NOT NULL,
+        CREATED_AT TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        IS_LOGIN BOOLEAN DEFAULT FALSE,
+        )
+        '''
+        threading.Thread(target=execute_query, args=(query,)).start()
+        return True
+
+    @log_decorator
+    def logout(self):
         pass
