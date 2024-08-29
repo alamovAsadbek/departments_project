3f7b1f653e9ac9020073bc0536a3a85142ab7082
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-30 02:12:44 +0500

    Change create_department func

--- roles/company/department.py
+++ roles/company/department.py
@@ -7,13 +7,13 @@ from main_files.decorator.decorator_func import log_decorator
 class Department:
     @log_decorator
     def create_department(self):
-        active_company = threading.Thread(target=get_active, args=('companies',)).start()
+        print("Waiting...")
+        active_company = get_active('companies')
         name: str = input("Enter Department Name: ").strip()
         query = '''
         INSERT INTO departments(COMPANY_ID, NAME) VALUES(%s, %s)
         '''
-        print(active_company)
         params = (active_company['id'], name,)
         threading.Thread(target=execute_query, args=(query, params)).start()
-        print('Department has been created')
+        print(f'{name} has been created')
         return True
