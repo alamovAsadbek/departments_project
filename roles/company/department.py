b583e1ac70bc3e92c3c5cd2c432e234f1d615529
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-08-30 01:52:18 +0500

    Create create_department func in department.py

--- roles/company/department.py
+++ roles/company/department.py
@@ -1,7 +1,18 @@
+import threading
+
+from main_files.database.db_setting import get_active, execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Department:
     @log_decorator
     def create_department(self):
-        pass
+        active_company = get_active('companies')
+        name: str = input("Enter Department Name: ").strip()
+        query = '''
+        INSERT INTO departments(COMPANY_ID, NAME) VALUES(%s, %s)
+        '''
+        params = (active_company['id'], name,)
+        threading.Thread(target=execute_query, args=(query, params)).start()
+        print('Department has been created')
+        return True
